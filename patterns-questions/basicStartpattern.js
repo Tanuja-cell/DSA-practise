fddbc690b05d721bb8df3389d464a5fd240f47d1
Tanuja-cell <hnytanu@gmail.com>
2026-08-12 16:01:20 +0530

    basic square pattern

--- patterns-questions/basicStartpattern.js
+++ patterns-questions/basicStartpattern.js
@@ -53,4 +53,30 @@ function decreasingTrianglePattern(n){
         console.log(row)
     }
 }
-decreasingTrianglePattern(4)
+// decreasingTrianglePattern(4)
+
+
+// 4. Right-aligned triangle
+
+//         *
+//       * *
+//     * * *
+//   * * * *
+// * * * * *
+
+
+
+function rightAllignedTrianglePattern(n){
+    for(let i = 0; i < n ; i++){
+        let row = "";
+        for(let j = 0 ; j < n - (i+1)  ; j++){
+         row = row + "-"
+        }
+        for(k = 0 ; k < i+1; k++){
+            row = row + "*";
+        }
+        console.log(row)
+    }
+}
+
+rightAllignedTrianglePattern(4)
