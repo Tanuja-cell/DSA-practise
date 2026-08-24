63ccd309c0ff6731deaeccc3176481c8e4bcbbcc
Tanuja-cell <hnytanu@gmail.com>
2026-08-24 19:21:00 +0530

    worked on search both linear and binary search

---
+++ searchAndSorting/binarySearch.js
@@ -0,0 +1,21 @@
+
+const binarySearch=(arr,targetElem)=>{
+    let leftPointer = 0;
+    let rightPointer = arr.length-1;
+     let middle;
+
+  while (leftPointer <= rightPointer){
+    middle = Math.floor((leftPointer + rightPointer)/2)
+    if(arr[middle] == targetElem){
+        return middle;
+    }
+    else if(arr[middle] < targetElem){
+        leftPointer = middle+1;
+    }else{
+        rightPointer = rightPointer -1;
+    }
+  }
+  return middle
+}
+
+console.log(binarySearch([5],5))
