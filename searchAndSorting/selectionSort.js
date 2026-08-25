1979a455506b404a0f8e1aed792c179919e82887
Tanuja-cell <hnytanu@gmail.com>
2026-08-25 19:52:39 +0530

    worked on selection sort

---
+++ searchAndSorting/selectionSort.js
@@ -0,0 +1,19 @@
+
+
+
+const selectionSort = (arr) => {
+
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[minIndex] > arr[j]) {
+                minIndex = j
+            }
+        }
+        let temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+    console.log("arr", arr)
+}
+selectionSort([2, 5, 4, 1])
